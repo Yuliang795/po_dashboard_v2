a0565b29f963c1026f35253a5efd88f3509f5788
Yuliang795 <yl.song@mail.utoronto.ca>
2023-07-18 20:33:35 -0400

    v0.2 flowchart

---
+++ dashboard.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os, sys, re, math
+from matplotlib.lines import Line2D
+import streamlit as st
+from st_aggrid import AgGrid,GridOptionsBuilder,ColumnsAutoSizeMode
+
+from utils import *
+st.set_page_config(layout="wide")
+
+main_df_path='main_df.csv'
+verify_df_path = 'verify_df.csv'
+p2_sp_df_path='2p_smt_s1732_2352_3556_r1.csv'
+main_df = get_df_cash(main_df_path)
+verify_df = get_df_cash(verify_df_path)
+p2_sp = get_df_cash(p2_sp_df_path)
+# verify_df = pd.read_csv('verify_df.csv')
+# p2_sp = pd.read_csv('2p_smt_s1732_2352_3556.csv')
+# p2_sp = p2_sp[p2_sp['cl_ml_ratio']==1]
+# # objective lm-lp
+# p2_sp['obj_value']=p2_sp['lambda_minus']-p2_sp['lambda_plus']
+# main_df['obj_value'] = main_df['lp_lambda_minus']-main_df['lp_lambda_plus']
+
+#
+kappa_set=np.sort(main_df['kappa'].unique()).tolist()
+data_set = list(main_df['data'].unique())+['all']
+seed_set = list(main_df['seed'].unique())+['all']
+
+st.title("Pareto Optimal Report")
+st.subheader('Pareto Optimal Process')
+st.image('./img/po_process.png')
+
+st.subheader('pivot table')
+AgGrid(main_df, height=400,columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS)
+st.write('#')
+
+# with open("./img/po_process.svg", "r") as f:
+#     svg_string = f.read()
+
+
+# f1_plot_tab, f1_df_tab = st.tabs(["visual", "pivot table"])
+### --- Plot the ARI vs. Kappa plot
+st.subheader("Pareto OptimalARI vs. Kappa plot")
+p1_container = st.container()
+
+
+
+with p1_container:
+    p1_col1,p1_col2,p1_col3 = st.columns([1,3,4])
+    with p1_col1:
+        data_slct = st.radio("data", data_set, key="data_slct_p1", horizontal=True)
+        seed_slct = st.radio("seed", seed_set, key="seed_slct_p1", horizontal=True)
+        # po_line_2p(main_df,verify_df, target_data='iris', target_seed=1732)
+        data_slct=data_slct if data_slct!='all' else None
+        seed_slct=seed_slct if seed_slct!='all' else None
+    with p1_col2:
+        ari_po_2p(main_df,p2_sp, target_data=data_slct, target_seed=seed_slct)
+        st.write('figure (1-a)')
+        po_MinMax_obj_2p(main_df,p2_sp, target_data=data_slct, target_seed=seed_slct)
+        st.write('figure (1-c)')
+    with p1_col3:
+        if data_slct!=None and seed_slct!=None:
+            po_line_2p(main_df,verify_df, target_data=data_slct, target_seed=seed_slct, p2_sp=p2_sp)
+            st.write('figure (1-b)')
+        else:
+            st.write('figure (1-b) pareto optimal line requires specific data and seed')
+
+# with p1_df_container:
+#     AgGrid(ptab_df, height=400)
+
+
+### --- Plot the pareto optimal line
+st.markdown('#')
+st.markdown('--')
+st.subheader("Pareto Optimal line")
+container = st.container()
+with container:
+    col1,col2,col3 = st.columns([1,3,1])
+    with col1:
+        p2_data_slct = st.radio("data", data_set[:-1], key="data_slct_p2", horizontal=True)
+        p2_seed_slct = st.radio("seed", seed_set[:-1], key="seed_slct_p2", horizontal=True)
+    with col2:
+        po_line_2p(main_df,verify_df, target_data=p2_data_slct, target_seed=p2_seed_slct, p2_sp=p2_sp)
+        st.write('figure (2)')
+
+### --- Plot the pareto optimal line
+st.markdown('#')
+st.markdown('--')
+st.subheader("Pareto Optimal obj value vs. Kappa")
+st.write(r"$\text{object value} = \lambda^- - \lambda^+$")
+p3_container = st.container()
+with p3_container:
+    p3_col1,p3_col2,p3_col3 = st.columns([1,3,2])
+    with p3_col1:
+        p3_data_slct = st.radio("data", data_set, key="data_slct_p3", horizontal=True)
+        p3_seed_slct = st.radio("seed", seed_set, key="seed_slct_p3", horizontal=True)
+    with p3_col2:
+        po_sum_time_2p(main_df, p2_sp,target_data=p3_data_slct, target_seed=p3_seed_slct)
+        
+        st.write('figure (3)')
+
---
+++ utils.py
@@ -0,0 +1,220 @@
+
+import numpy as np
+import pandas as pd
+import os,sys,re,math
+import matplotlib.pyplot as plt
+import streamlit as st
+
+@st.cache_data
+def query_main_verify_df(df, target_data, target_seed, target_kappa):
+  return df[(df['data']==target_data)\
+          &(df['seed']==target_seed)\
+          &(df['kappa']==target_kappa)].copy()
+
+def po_line_2p(main_df, verify_df, target_data, target_seed, p2_sp=None):
+  kappa_set=np.sort(main_df['kappa'].unique()).tolist()
+  # create fig
+  fig, axs = plt.subplots(3, 3, figsize=(10, 10))
+  axs = axs.ravel()
+  for ax_ind, ax in enumerate(axs):
+    curr_maindf = query_main_verify_df(main_df,target_data, target_seed, target_kappa=kappa_set[ax_ind])
+    curr_verifydf = query_main_verify_df(verify_df,target_data, target_seed, target_kappa=kappa_set[ax_ind])
+    curr_verify_lm = curr_verifydf[curr_verifydf['verify']=='lm']
+    curr_verify_lp = curr_verifydf[curr_verifydf['verify']=='lp']
+
+    ax.scatter(curr_maindf['lp_lambda_minus'], curr_maindf['lp_lambda_plus'], label=f'opt_lambda_plus',marker='o', facecolors='none',edgecolors='b',linewidths=1.5, zorder=10)
+    ax.scatter(curr_maindf['lm_lambda_minus'], curr_maindf['lm_lambda_plus'], label=f'opt_lambda_minus',marker='x', color='darkorange', zorder=5)
+    ax.scatter(curr_verify_lm['p2_lm_b0'], curr_verify_lm['p2_lm_b1'], label=f'MinLm', marker='^', color='g',zorder=5)
+    ax.scatter(curr_verify_lp['p2_lp_b0'], curr_verify_lp['p2_lp_b1'], label=f'MaxLp',marker='v', color='r',zorder=5)
+    ## add 2p
+    #
+    if p2_sp is not None:
+      print(f"********************** {p2_sp is not None}   ----------- {p2_sp.shape}")
+      curr_2pdf = query_main_verify_df(p2_sp,target_data, target_seed, target_kappa=kappa_set[ax_ind])
+      ax.scatter(curr_2pdf['lambda_minus'], curr_2pdf['lambda_plus'], marker='+', color='black',zorder=15,s=100, label='2p')
+
+    # Adjust y-axis limits with padding
+    lp_list = [*curr_maindf['lm_lambda_plus'],*curr_maindf['lp_lambda_plus'],*curr_verifydf['p2_lm_b1'],*curr_verifydf['p2_lp_b1']]
+    lp_min = min(lp_list) if len(lp_list)>0 else 0
+    lp_max = max(lp_list) if len(lp_list)>0 else 0
+    y_padding = (lp_min*0.05+ lp_max*0.05)/2
+    if y_padding>0:
+      ax.set_ylim(lp_min - y_padding, lp_max + y_padding)
+
+    # Set labels and title for each subplot
+    ax.set_title(f'kappa={kappa_set[ax_ind]}', fontsize=8)
+
+  # add label
+  for xlab_ind in [6,7,8]:
+    axs[xlab_ind].set_xlabel('lambda_minus')
+  for ylab_ind in [0,3,6]:
+    axs[ylab_ind].set_ylabel('lambda_plus')
+
+  # add legend
+  axs[2].legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
+  axs[5].legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
+  axs[8].legend(loc='center left', bbox_to_anchor=(1.05, 0.5))
+
+  # Add a big title to the entire set of plots
+  fig.suptitle(f'{target_data} seed: {target_seed}  lambda_minus vs, lambda_plus', fontsize=16,y=1)
+  # Adjust spacing and layout
+  plt.tight_layout()
+
+  # Display the plot
+#   plt.show()
+  st.pyplot(fig)
+
+
+## ARI line PO
+@st.cache_data
+def groupby_df_ari(df, target_data=None, target_seed=None):
+  tmp_df = df.copy()
+  target_col_dict = {k: v for k, v in zip(['data','seed'], [target_data, target_seed]) if v!=None}
+  for k,v in target_col_dict.items():
+    tmp_df = tmp_df[tmp_df[k]==v]
+  res =  tmp_df.groupby(by=list(target_col_dict.keys())+['kappa'], as_index=False)\
+  .agg({'ARI':[('_avg','mean'), ('_min', 'min'), ('_max', 'max')]}).copy()
+  res.columns = ['{}{}'.format(col, agg) for col, agg in res.columns]
+  return res
+@st.cache_data
+def groupby_df_general_avg_ari(df,target_data=None, target_seed=None):
+  tmp_df = df.copy()
+  target_col_dict = {k: v for k, v in zip(['data','seed'], [target_data, target_seed]) if v!=None}
+  for k,v in target_col_dict.items():
+    tmp_df = tmp_df[tmp_df[k]==v]
+  res = tmp_df.groupby(['kappa','data','seed'],as_index=False)\
+    .agg({'ARI':[('_avg','mean'), ('_min', 'min'), ('_max', 'max')]})
+  res.columns = ['{}{}'.format(col, agg) for col, agg in res.columns]
+  return res.groupby(by=list(target_col_dict.keys())+['kappa'],as_index=False).mean(numeric_only=True)
+@st.cache_data
+def pareto_sol_count(df,target_data,target_seed ):
+  tmp_df = df.copy()
+  tmp_df['pareto_index'] = tmp_df['pareto_index']+1
+  target_col_dict = {k: v for k, v in zip(['data','seed'], [target_data, target_seed]) if v!=None}
+  for k,v in target_col_dict.items():
+      tmp_df = tmp_df[tmp_df[k]==v]
+
+  num_po_res = tmp_df.groupby(by=['kappa','data','seed'], as_index=False).max()
+  return num_po_res.groupby(by=list(target_col_dict.keys())+['kappa'],as_index=False).mean(numeric_only=True)
+
+
+def ari_po_2p(main_df,p2_sp,target_data,target_seed):
+
+  kappa_set=np.sort(main_df['kappa'].unique()).tolist()
+  if target_data!=None and target_seed!=None:
+    # tmp_maindf_group_ari = groupby_df_ari(main_df, target_data, target_seed)
+    tmp_maindf_group_ari = groupby_df_general_avg_ari(main_df,target_data ,target_seed)
+  else:
+    tmp_maindf_group_ari = groupby_df_general_avg_ari(main_df,target_data ,target_seed)
+  tmp_p2_group_ari = groupby_df_ari(p2_sp, target_data, target_seed)
+
+  fig, ax = plt.subplots()
+  po_avg_ari, = ax.plot(tmp_maindf_group_ari['kappa'], tmp_maindf_group_ari['ARI_avg'],  color='darkblue', label='po_avg_ari',zorder=10)
+  po_min_ari, = ax.plot(tmp_maindf_group_ari['kappa'], tmp_maindf_group_ari['ARI_min'], linestyle='--',dashes=(3, 3),color='lightsteelblue', label='po_min_ari',zorder=15)
+  po_max_ari, = ax.plot(tmp_maindf_group_ari['kappa'], tmp_maindf_group_ari['ARI_max'], linestyle='--',dashes=(3, 3),color='lightsteelblue', label='po_max_ari',zorder=15)
+  # add count of pareto optimal solution
+  # add number of pareto optimal solution count
+  pareto_sol_count_df = pareto_sol_count(main_df,target_data,target_seed)
+  for ind in range(tmp_maindf_group_ari.shape[0]):
+    ax.text(tmp_maindf_group_ari.loc[ind, 'kappa'],
+    tmp_maindf_group_ari.loc[ind,'ARI_avg'],
+    f'{round(pareto_sol_count_df.loc[ind, "pareto_index"],1)}',
+    ha='center', va='bottom')
+  
+  # 2p sp avg
+  p2_sp_avg, = ax.plot(tmp_p2_group_ari['kappa'], tmp_p2_group_ari['ARI_avg'],color='darkred', label='2p_avg')
+
+  # Set the legend with different styles for each line
+  ax.legend(handles=[po_avg_ari, po_max_ari,p2_sp_avg],
+            labels=['po_avg_ari', 'po_max&min_ari','2p_avg_ari'],
+            loc='lower right')
+  # Add labels and title
+  ax.set_xlabel('kappa')
+  ax.set_ylabel('ARI')
+  ax.set_title(f'ARI vs. Kappa -Data:{["all data avg",target_data][target_data!=None]}, Seed:{["all seeds avg",target_seed][target_seed!=None]}')
+
+#   plt.show()
+  st.pyplot(fig)
+
+
+
+
+### solver time
+@st.cache_data
+def groupby_df_general_horrizontal(df,col_to_agg, target_data=None, target_seed=None):
+  tmp_df = df.copy()
+  target_col_dict = {k: v for k, v in zip(['data','seed'], [target_data, target_seed]) if v!=None}
+  for k,v in target_col_dict.items():
+    tmp_df = tmp_df[tmp_df[k]==v]
+  res = tmp_df.groupby(['kappa','data','seed'],as_index=False)\
+    .agg({col_to_agg:[('_avg','mean'), ('_min', 'min'), ('_max', 'max'),('_sum', 'sum')]})
+  res.columns = ['{}{}'.format(col, agg) for col, agg in res.columns]
+  return res.groupby(by=list(target_col_dict.keys())+['kappa'],as_index=False).mean(numeric_only=True)
+@st.cache_data
+def groupby_df_general_vertical(df, col_to_agg, target_data=None, target_seed=None):
+  tmp_df = df.copy()
+  target_col_dict = {k: v for k, v in zip(['data','seed'], [target_data, target_seed]) if v!=None}
+  for k,v in target_col_dict.items():
+    tmp_df = tmp_df[tmp_df[k]==v]
+  res =  tmp_df.groupby(by=list(target_col_dict.keys())+['kappa'], as_index=False)\
+  .agg({col_to_agg:[('_avg','mean'), ('_min', 'min'), ('_max', 'max'), ('_sum', 'sum')]}).copy()
+  res.columns = ['{}{}'.format(col, agg) for col, agg in res.columns]
+  return res
+
+def po_sum_time_2p(main_df, p2_sp,target_data,target_seed ):
+  kappa_set=np.sort(main_df['kappa'].unique()).tolist()
+
+  tmp_maindf_group_ari = groupby_df_general_vertical(main_df,col_to_agg='total_slvr_time',target_data=target_data, target_seed=target_seed)
+  tmp_p2_group_ari = groupby_df_general_vertical(p2_sp,col_to_agg='total_slvr_time', target_data=target_data, target_seed=target_seed)
+
+  fig, ax = plt.subplots()
+  po_avg_ari, = ax.plot(tmp_maindf_group_ari['kappa'], tmp_maindf_group_ari['total_slvr_time_sum'], color='darkblue', label='po_avg_ari')
+
+  # 2p sp avg
+  p2_sp_avg, = ax.plot(tmp_p2_group_ari['kappa'], tmp_p2_group_ari['total_slvr_time_avg'],color='darkred', label='2p_avg')
+
+  # Set the legend with different styles for each line
+  ax.legend(handles=[po_avg_ari,p2_sp_avg],
+            labels=['po_sum_time','2p_sum_time'],
+            loc='lower right')
+  # Add labels and title
+  ax.set_xlabel('kappa')
+  ax.set_ylabel('time (s)')
+  ax.set_title(f'Total Solver Time vs. Kappa -data:{["all data avg",target_data][target_data!=None]} seed:{["all seeds avg",target_seed][target_seed!=None]}')
+
+#   plt.show()
+  st.pyplot(fig)
+
+
+
+### Plot pareto optimal objective values MinMax
+def po_MinMax_obj_2p(main_df, p2_sp,target_data,target_seed):
+    kappa_set=np.sort(main_df['kappa'].unique()).tolist()
+    #
+    tmp_maindf_group_obj = groupby_df_general_horrizontal(main_df,col_to_agg='obj_value',target_data=target_data, target_seed=target_seed)
+    tmp_p2_group_obj = groupby_df_general_horrizontal(p2_sp,col_to_agg='obj_value', target_data=target_data, target_seed=target_seed)
+    #
+    fig, ax = plt.subplots()
+    po_avg_obj, = ax.plot(tmp_maindf_group_obj['kappa'], tmp_maindf_group_obj['obj_value_avg'], color='darkblue', label='po_avg')
+    po_min_obj, = ax.plot(tmp_maindf_group_obj['kappa'], tmp_maindf_group_obj['obj_value_min'], linestyle='--',dashes=(3, 3),color='lightsteelblue', label='po_min_obj',zorder=15)
+    po_max_obj, = ax.plot(tmp_maindf_group_obj['kappa'], tmp_maindf_group_obj['obj_value_max'], linestyle='--',dashes=(3, 3),color='lightsteelblue', label='po_max_obj',zorder=15)
+
+    # 2p sp avg
+    p2_sp_avg, = ax.plot(tmp_p2_group_obj['kappa'], tmp_p2_group_obj['obj_value_avg'],color='darkred', label='2p_avg')
+
+    # Set the legend with different styles for each line
+    ax.legend(handles=[po_avg_obj,po_min_obj,p2_sp_avg],
+              labels=['po_avg_obj', 'po_Min&Max_obj','2p_avg_obj'],
+              loc='lower right')
+    # Add labels and title
+    ax.set_xlabel('kappa')
+    ax.set_ylabel('objective value')
+    ax.set_title(f'Objective vs. Kappa -data:{["all data avg",target_data][target_data!=None]} seed:{["all seeds avg",target_seed][target_seed!=None]}')
+
+    # plt.show()
+    st.pyplot(fig)
+
+@st.cache_data
+def get_df_cash(path):
+  df = pd.read_csv(path)
+  return df
